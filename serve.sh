78c2920e772af4668114c115fa272737324afee7
Jonathan Knapp <jon@coffeeandcode.com>
2026-07-24 18:14:06 -0400

    Do not cache in dev

--- serve.sh
+++ serve.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-podman run --rm -p 8000:80 -v "$(pwd)/public:/usr/share/nginx/html:z" nginx:alpine
+podman run --rm -p 8000:80 \
+  -v "$(pwd)/public:/usr/share/nginx/html:z" \
+  -v "$(pwd)/nginx.dev.conf:/etc/nginx/conf.d/default.conf:z" \
+  nginx:alpine
